baaccff19b897374b0eb3afa007a2ece192a53dd
Ramón Martínez Nieto <ramon.martinez.nieto@gmail.com>
2020-09-01 18:24:24 +0200

    Add pause

---
+++ Assets/Scripts/Pause.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class Pause : MonoBehaviour
+{
+    public static bool GameIsPaused = false;
+
+    public GameObject MenuEsc;
+    public GameObject MenuGameOver;
+
+    private void Update()
+    {
+        if (MenuEsc.activeSelf || MenuGameOver.activeSelf)
+        {
+            PauseGame();
+        }
+        else 
+        {
+            ResumeGame();
+        }
+    }
+
+    public void PauseGame()
+    {
+        GameIsPaused = true;
+        Time.timeScale = 0f; 
+    }
+
+    public void ResumeGame()
+    {
+        GameIsPaused = false;
+        Time.timeScale = 1f;
+    }
+}
