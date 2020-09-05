93994cfd55040df1a4d86d3b0d50a3b71e41954d
Ramón Martínez Nieto <ramon.martinez.nieto@gmail.com>
2020-09-05 12:51:21 +0200

    add dying effect

---
+++ Assets/Scripts/UIElements/ControlDyingEffect.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ControlDyingEffect : MonoBehaviour
+{
+    public PlayerStats playerStats; 
+    private RawImage dyingEffect;
+
+    private void Awake()
+    {
+        dyingEffect = GetComponent<RawImage>(); 
+    }
+
+    private void Start()
+    {
+        dyingEffect.color = new Color(1f, 1f, 1f, 0f);
+    }
+
+    void Update()
+    {
+        var healt = playerStats.CurrentHealt;
+
+        if (healt <= 50)
+        {
+            float alpha = 0.6f - ((float)healt / 100);
+            dyingEffect.color = new Color(1f, 1f, 1f, alpha);
+        }
+        else 
+        {
+            dyingEffect.color = new Color(1f, 1f, 1f, 0f);
+        }
+        
+    }
+}
